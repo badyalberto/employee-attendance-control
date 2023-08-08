31fc347043129e2a8233027cd31b39d407feae83
Alberto Chica Santillana <achica@kenostechnology.com>
2023-08-08 20:12:56 +0200

    Changes: Startup and UserController

--- ms.users.api/Controllers/UsersController.cs
+++ ms.users.api/Controllers/UsersController.cs
@@ -1,6 +1,29 @@
-﻿namespace ms.users.api.Controllers
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ms.users.application.Commands;
+using ms.users.application.Queries;
+using ms.users.application.Requests;
+using System.Threading.Tasks;
+
+namespace ms.users.api.Controllers
 {
-    public class UsersController
+    [Authorize(Roles = "admin")]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator) => _mediator = mediator;
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetAllUsers() => Ok(await _mediator.Send(new GetAllUsersQuery()));
+
+        public async Task<IActionResult> CreateAccount([FromBody] AccountRequest account) =>
+            Ok(await _mediator.Send(new CreateUserAccountCommand(account.UserName,account.Password,account.Role)));
+
+
     }
 }
--- ms.users.api/Startup.cs
+++ ms.users.api/Startup.cs
@@ -99,25 +99,19 @@ namespace ms.users.api
                 });
 
                 swagger.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                  {
+                {
                     {
                       new OpenApiSecurityScheme
                       {
                         Reference = new OpenApiReference
-                          {
+                        {
                             Type = ReferenceType.SecurityScheme,
                             Id = "Bearer"
-                          },
-                          Scheme = "oauth2",
-                          Name = "Bearer",
-                          In = ParameterLocation.Header,
+                        }
 
-                        },
-                        new List<string>()
-                      }
-                    });
-
-
+                      },new string[] { }
+                    }
+                });
             });
         }
 
