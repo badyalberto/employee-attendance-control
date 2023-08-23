6e3ab06ed22e1c72e0033cd5a724362fd234272f
Alberto Chica Santillana <achica@kenostechnology.com>
2023-08-23 17:31:16 +0200

    Added: Añadida la capa de presentacion (Employee)

--- ms.employees/ms.employees.api/Startup.cs
+++ ms.employees/ms.employees.api/Startup.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,10 +9,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using ms.employees.application.Mappers;
+using ms.employees.application.Queries;
+using ms.employees.domain.Repositories;
+using ms.employees.infrastructure.Data;
+using ms.employees.infrastructure.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ms.employees.api
@@ -28,9 +39,71 @@ namespace ms.employees.api
         {
 
             services.AddControllers();
+            services.AddScoped(typeof(IDapperContext), typeof(EmployeesDapperContext));
+            services.AddScoped(typeof(IEmployeeRepository), typeof(EmployeeRepository));
+
+            var automapperConfig = new MapperConfiguration(mapperConfig =>
+            { mapperConfig.AddMaps(typeof(EmployeesMapperProfile).Assembly); });
+            IMapper mapper = automapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddMediatR(typeof(GetAllEmployeeQuery).GetTypeInfo().Assembly);
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ms.employees.api", Version = "v1" });
+            });
+
+            var privateKey = Configuration.GetValue<string>("Authentication:JWT:Key");
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(privateKey)),
+                    ValidateLifetime = true,
+                    RequireExpirationTime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Employees Attendance Api",
+                    Version = "v1"
+                });
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Description = "Cabecera de autorización JWT. \r\n Introduzca ['Bearer'] [espacio] [Token]"
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },new string[] { }
+                    }
+                });
             });
         }
 
@@ -48,12 +121,16 @@ namespace ms.employees.api
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json","Employees Attendance API V1"));
         }
     }
 }
